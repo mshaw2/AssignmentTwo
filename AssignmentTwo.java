5fe609f8c65eb431f89aeb6c98e6a7cb4e04c102
Manmohan Shaw <shawmanmohan@gmail.com>
2016-06-22 10:22:02 +0530

    SessionOne: AssignmentTwo

---
+++ AssignmentTwo.java
@@ -0,0 +1,9 @@
+package com.sessionone;
+/*Declare three integer variables (a,b,c) in a single declaration 
+ * and simultaneously initialize b to 4.
+ * 
+ */
+
+public class AssignmentTwo {
+	int a,b=4,c;
+}
